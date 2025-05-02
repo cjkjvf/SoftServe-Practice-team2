54517ce6e4bd70bb8e49b054eb80e3265c1120ae
Pavlo Sam <serpavlo2005@gmail.com>
2025-05-02 21:20:25 +0300

    Add Footer and Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
- import Header from './components/Header';
- import Footer from './components/Footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
+    <div className="page-layout">
       <Header />
       <main className="main-content">
-        <h1>Кіноафіша</h1>
-        {/* Далі буде контент */}
+        {/* Сторінки */}
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
+
 
 export default App;
