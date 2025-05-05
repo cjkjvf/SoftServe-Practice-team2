1355a224497e8e74f974346499a7138e0bfd3b9a
Pavlo Sam <serpavlo2005@gmail.com>
2025-05-06 02:52:52 +0300

    create new branch

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import "../styles/Header.css";
+import { Search, User, Menu } from "lucide-react";
+
+export default function Header() {
+  return (
+    
+    <header className="hero section">
+
+      <div className="hero-nav">
+        <div className="logo">MIRAGE</div> 
+        <nav className="nav-icons">
+          <span className="icon-circle"><Search size={20} /></span>
+          <span className="icon-circle"><User size={20} /></span>
+          <span className="icon-circle"><Menu size={20} /></span>
+        </nav>
+      </div>
+    </header>
+  );
+}
