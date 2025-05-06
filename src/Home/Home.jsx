862ca982f4e6b9027af339515dae4b0352d685bd
Pavlo Sam <serpavlo2005@gmail.com>
2025-05-07 02:06:39 +0300

    Slider working

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import './Home.css'
+import React from 'react';
+import './Home.css';
 import PromoSlider from './PromoSlider';
 
 const Home = () => {
   return (
     <div className='home'>
-       <PromoSlider />
-  
+      <PromoSlider />
+      
+      {/* Додатковий контент після слайдера */}
+      <div className="additional-content">
+        <h2>Заголовок контенту після слайдера</h2>
+        <p>
+          Це додатковий текст або інформація, яку можна додати під слайдером. 
+          Тут ви можете помістити будь-який контент, наприклад, текст, зображення чи інші компоненти.
+        </p>
+        <button className="cta-button">Дізнатись більше</button>
+      </div>
+
+      {/* Інший контент можна додати тут */}
+      <div className="extra-section">
+        <h3>Ще один розділ</h3>
+        <p>
+          Цей розділ можна використати для ще більшого контенту або для якихось конкретних 
+          пропозицій або новин.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/Home/PromoSlider.jsx
+++ src/Home/PromoSlider.jsx
@@ -1,7 +1,8 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import { Navigation } from 'swiper/modules';
+import 'swiper/css/autoplay'
+import { Autoplay, Navigation } from 'swiper/modules';
 import './PromoSlider.css';
 
 const slides = [
@@ -9,27 +10,37 @@ const slides = [
     image: 'src/assets/minecraft.png',
   },
   {
-    image: 'src/assets/mission-impossible.png', // Додайте ще зображення для слайдера
+    image: 'src/assets/mission-impossible.png',
   },
   {
     image: 'src/assets/One-Punch-Man.png',
   },
-
   {
     image: 'src/assets/Clown-Cornfield.png',
   },
-  
   {
     image: 'src/assets/Serfer.png',
+  },
+  {
+    image: 'src/assets/Shaleniy-copu.png',
+  },
+  {
+    image: 'src/assets/Pynct-priznachenya-rodove-proclyatya.png',
   },
 ];
 
 export default function PromoSlider() {
   return (
     <Swiper
+      modules={[Navigation,Autoplay]}
       navigation
-      modules={[Navigation]}
-      autoplay={{ delay: 3000 }}  // Автоматичне перемикання кожні 3 секунди
+      autoplay={{
+        delay: 3000,  // Автоматичне перемикання кожні 3 секунди
+        disableOnInteraction: false,  // Автоматичне перемикання не зупиняється при взаємодії
+      }}
+      direction="vertical" // Вертикальний слайдер
+      loop={true} // Безкінечний цикл слайдів
+      initialSlide={0}
       className="promo-slider"
     >
       {slides.map((slide, index) => (
@@ -41,7 +52,6 @@ export default function PromoSlider() {
             <div className="slide-overlay">
               <div className="slide-info">
                 <h1>Title of the Slide</h1>
-                {/* Можна додати більше інформації тут */}
               </div>
             </div>
           </div>
@@ -50,5 +60,4 @@ export default function PromoSlider() {
     </Swiper>
   );
 }
-
 
