0587d3495e57e66e042d28eb4b46d9db2a709500
Pavlo Sam <serpavlo2005@gmail.com>
2025-05-13 18:53:24 +0300

    fixed, should be ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Home/Home';
+import Layout from './components/Layout';
+import About from './pages/About'; // Це просто для тесту, можеш потім видалити
+import './global.css';
+import './styles/index.css';
 
 function App() {
   return (
-    <div className="page-layout">
-      <Header />
-      <main className="main-content">
-        {/* Сторінки */}
-      </main>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} /> 
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 
 export default App;
