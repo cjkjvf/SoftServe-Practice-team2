7824e3343ac12746e0b8a00cfaf45af095fe1aab
Mykola Buvanyk <ogne007@gmail.com>
2025-05-19 22:53:45 +0300

    Refactor routing structure in App component for improved organization and clarity

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,10 @@ import About from './pages/About';
 import Favorites from "./pages/Favorites";
 import SeatPlanPay from "./choice/seatplan-pay";
 import MovieDetails from './pages/MovieDetails';
-import Login from './Autorize/Login'; 
+import Login from './Autorize/Login';
 import Register from './Autorize/Register';
 import ListFilms from './Home/ListFilms';
-import Catalog from './Catalog/Catalog';
-import ScrollToTop from './ScrollToTop';
+// import Catalog from './Catalog/Catalog';
 import './global.css';
 import './styles/index.css';
 import AdminLogin from './pages/Admin/pages/Login';
@@ -23,24 +22,65 @@ import ProtectedRoute from './pages/Admin/components/ProtectedRoute';
 function App() {
   return (
     <BrowserRouter>
-      <ScrollToTop />
-        <Routes>
-          {/* Головна обгортка з Layout */}
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="favorites" element={<Favorites />} />
-            <Route path="movies/:movieId" element={<MovieDetails />} />
-            <Route path="/catalogfilm" element={<CatalogFilm/>}/>
-            <Route path="listfilms" element={<ListFilms />} />
-            <Route path="catalog" element={<Catalog />} />
-          </Route>
+      <Routes>
+        {/* Головна обгортка з Layout */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="favorites" element={<Favorites />} />
+          <Route path="movies/:movieId" element={<MovieDetails />} />
+          <Route path="listfilms" element={<ListFilms />} />
+          {/* <Route path="catalog" element={<Catalog />} /> */}
+        </Route>
 
         {/* Маршрути поза Layout (без header/footer) */}
         <Route path="seatplan-pay" element={<SeatPlanPay />} />
         <Route path="seatplan" element={<SeatPlanPay />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
+        <Route path="/admin" element={<Navbar />}>
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="movies"
+            element={
+              <ProtectedRoute>
+                <Movies />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="casts"
+            element={
+              <ProtectedRoute>
+                <Casts />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="halls"
+            element={
+              <ProtectedRoute>
+                <Halls />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="screenings"
+            element={
+              <ProtectedRoute>
+                <Screenings />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+        <Route path="/admin/login" element={<AdminLogin />} />
       </Routes>
     </BrowserRouter>
   );
